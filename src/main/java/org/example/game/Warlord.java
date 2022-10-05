40496aa687b8f01d74c8eb2ab1c2b257bca1d760
Tomasz Borowski <bortom112233@gmail.com>
2022-10-05 19:44:56 +0200

    Warlord extends Defender

--- src/main/java/org/example/game/Warlord.java
+++ src/main/java/org/example/game/Warlord.java
@@ -6,13 +6,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Warlord extends Warrior {
-
-    private int defense = 2;
+public class Warlord extends Defender {
 
     protected Warlord() {
-        super(4);
-        setInitialHp(100);
+        super(4, 100, 2);
     }
 
     Iterable<Warrior> arrangePositions(Iterator<Warrior> iterator) {
@@ -141,22 +138,12 @@ public class Warlord extends Warrior {
         }
     }
 
-
-    @Override
-    public void receiveDamage(HasAttack damager) {
-        int finalDamage = Math.max(0, damager.getAttack() - getDefense());
-        super.receiveDamage(() -> finalDamage);
-    }
-
     @Override
     void equipWeapon(Weapon weapon) {
         this.setInitialHp(getInitialHP() + weapon.getHp());
         this.setAttack(getAttack() + weapon.getAttack());
-        this.defense = getDefense() + weapon.getDefence();
+        setDefense(getDefense() + weapon.getDefence());
         addWeaponToList(weapon);
     }
 
-    int getDefense() {
-        return defense;
-    }
 }
